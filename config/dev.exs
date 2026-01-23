52f5fd7cd106fcfc31eeab54bb0d8ce74110d8e4
Carlos Montalvo <carlosmto@pm.me>
2026-01-23 18:52:30 -0500

    fix: allow external access by binding HTTP to all interfaces in development

--- config/dev.exs
+++ config/dev.exs
@@ -19,7 +19,7 @@ config :oban_app, ObanApp.Repo,
 config :oban_app, ObanWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}],
+  http: [ip: {0, 0, 0, 0}],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
